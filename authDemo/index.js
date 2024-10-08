f6464d5d04f6c0ccc7c76bd4a47224a9362aa817
parvanhiii <parvanisood24@gmail.com>
2024-10-09 01:22:56 +0530

    register

--- authDemo/index.js
+++ authDemo/index.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
+mongoose.connect('mongodb://127.0.0.1:27017/authDemo');
 
+const db=mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",()=>{
+    console.log("DB CONNECTED")
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({extended:true}));
 
+app.get('/', (req, res) => {
+    res.send('this is the home page'); 
+});
 app.get('/register', (req, res) => {
     res.render('register'); // Render the register.ejs file
 });
 
+app.post('/register', async(req, res) => {
+    const{password,username}=req.body;
+    const hash=await bcrypt.hash(password,12);
+    const user= new User({
+        username,
+        password:hash
+    })
+    await user.save();
+    res.redirect('/');
+});
 
 app.get('/secret', (req, res) => {
     res.send("This is a secret, login to see");
